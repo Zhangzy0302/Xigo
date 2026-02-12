84d35679803fc6aee720e9d234f9cec7e4d7aebc
zzy <2218847957@qq.com>
2026-02-12 10:37:23 +0800

    Initial Commit

---
+++ Xigo/XigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XigoApp.swift
+//  Xigo
+//
+//  Created by yangyang on 2026/2/12.
+//
+
+import SwiftUI
+
+@main
+struct XigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
